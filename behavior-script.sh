e81080029d5fa4a0d254e485ad2fbef6510b91f1
NEDA ANRES <nedaanres@neda.gov.ph>
2018-11-21 04:58:19 +0800

    Edited script to cater with appliance

--- behavior-script.sh
+++ behavior-script.sh
@@ -2,7 +2,7 @@ populations=(lcli lchi hcli hchi universe jomalig dinapigue)
 scenarios=(bau fish-catch fisher-revenue both)
 criteria=(baseline accessibility sufficiency adoption sustainability)
 
-str1='./netlogo-headless.sh --model ~/Desktop/commod-neda-refined/commod.nlogo --experiment '
+str1='~/Desktop/netlogo/netlogo-headless.sh --model ~/Desktop/commod-neda-refined/commod.nlogo --experiment '
 str2=' --threads 4 --spreadsheet ~/Desktop/commod-neda-refined/rpg_'
 str3='_results/'
 str4='.csv'
